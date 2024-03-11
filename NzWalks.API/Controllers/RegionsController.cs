8340d3ce4b2dc88d04fac056c8c5f159e1fe7607
ALISOLANO\franc <francisco.solano>
2024-03-10 22:50:20 -0300

    Itens do projeto

---
+++ NzWalks.API/Controllers/RegionsController.cs
@@ -0,0 +1,128 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NzWalks.API.Data;
+using NzWalks.API.Models.Domain;
+using NzWalks.API.Models.DTO;
+
+namespace NzWalks.API.Controllers
+{
+    // https://localhost:1234/api/regions
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionsController : ControllerBase
+    {
+        private readonly NzWalksDbContext dbContext;
+        public RegionsController(NzWalksDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        // GET ALL REGIONS
+        // GET: https://localhost:portnumber/api/regions
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            // Get Data From Database - Domain model
+            var regionsDomain = dbContext.Regions.ToList();
+
+            // Map Domain MNodels to DTOs
+            var regionsDto = new List<RegionDto>();
+            foreach (var region in regionsDomain)
+            {
+                regionsDto.Add(new RegionDto
+                {
+                    Id = region.Id,
+                    Code = region.Code,
+                    Name = region.Name,
+                    RegionImageUrl = region.RegionImageUrl
+                });
+            }
+
+            // Return DTOs
+            return Ok(regionsDto);
+        }
+
+        // GET SINGLE REGION (GET REGION BY ID)
+        // GET: https://localhost:portnumber/api/regions/{id}
+        [HttpGet]
+        [Route("{id:Guid}")]
+        public IActionResult GetById([FromRoute] Guid id)
+        {
+            // var region = dbContext.Regions.Find(id);
+            // Get Region Domain Model From Database
+            var regionDomain = dbContext.Regions.FirstOrDefault(x => x.Id == id);
+
+            if (regionDomain == null)
+            {
+                return NotFound();
+            }
+
+            // Map/Convert Region Domain Model To Region DTO
+            var regionDto = new RegionDto
+            {
+                Id = regionDomain.Id,
+                Code = regionDomain.Code,
+                Name = regionDomain.Name,
+                RegionImageUrl = regionDomain.RegionImageUrl
+            };
+
+            // return DTO back to client
+            return Ok(regionDto);
+        }
+
+        // POST To Create a new Region
+        // POST: https://localhost:portnumber/api/regions
+        [HttpPost]
+        public IActionResult Create([FromBody] AddRegionRequestDto addRegionRequestDto)
+        {
+            // Map or Convert DTO to Domain Model
+            var regionDomainModel = new Region
+            {
+                Code = addRegionRequestDto.Code,
+                Name = addRegionRequestDto.Name,
+                RegionImageUrl = addRegionRequestDto.RegionImageUrl
+            };
+
+            // Use Domain Model to Create Region
+            dbContext.Regions.Add(regionDomainModel);
+            dbContext.SaveChanges();
+
+            // Map Domain model back to Dto
+            var regionDto = new RegionDto
+            {
+                Id = regionDomainModel.Id,
+                Code = regionDomainModel.Code,
+                Name = regionDomainModel.Name,
+                RegionImageUrl = regionDomainModel.RegionImageUrl
+            };
+
+            return CreatedAtAction(nameof(GetById), new { id = regionDomainModel.Id }, regionDto);
+        }
+
+
+        #region old
+        //[HttpGet]
+        //public IActionResult GetAll()
+        //{
+        //    var regions = new List<Region>
+        //    {
+        //        new Region
+        //        {
+        //            Id = Guid.NewGuid(),
+        //            Name = "Auckland Region",
+        //            Code = "AKL",
+        //            RegionImageUrl = "https://images.pexels.com./photos/5169056/pexels-photo-5169056.jpeg?auto=compress&cs=tinysrgb&w=1260&h750&dpr=1"
+        //        },
+        //        new Region
+        //        {
+        //            Id = Guid.NewGuid(),
+        //            Name = "Wellington Region",
+        //            Code = "WLG",
+        //            RegionImageUrl = "https://images.pexels.com./photos/4350631/pexels-photo-4350631.jpeg?auto=compress&cs=tinysrgb&w=1260&h750&dpr=1"
+        //        },
+        //    };
+
+        //    return Ok(regions);
+        //}
+        #endregion
+    }
+}
---
+++ NzWalks.API/Controllers/StudentsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NzWalks.API.Controllers
+{
+    // GET: https://localhost:portnumber/api/studentes
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        // GET: https://localhost:portnumber/api/studentes
+        [HttpGet]
+        public IActionResult GetAllStudents()
+        {
+            string[] studenteNames = new string[] { "Jhon", "Jane", "Mark", "Emily", "David" };
+
+            return Ok(studenteNames);
+        }
+    }
+}
